3f27bb6020d3af9c2c2eaf7dbbeb391d526c2a84
pakdonghun <66273924+pakdonghun@users.noreply.github.com>
2020-12-29 23:21:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=py
 
 indeed_soup = BeautifulSoup(indeed_result.text,"html.parser")
 
-print(indeed_soup) 
+pagination = indeed_soup.find("div", {"class": "pagination"})
+
+pages = pagination.find_all('a')
+spans = []
+for page in pages:
+   spans.append(page.find("span"))
+print(spans[:-1])
